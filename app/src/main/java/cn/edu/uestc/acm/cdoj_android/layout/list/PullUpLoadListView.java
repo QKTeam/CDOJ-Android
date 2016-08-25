75c62d1babea0ef540797d718d7632247760bd05
great <leiguowei@outlook.com>
2016-08-26 00:16:52 +0800

    开始入手比赛页面，功能模块化

--- app/src/main/java/cn/edu/uestc/acm/cdoj_android/layout/list/PullUpLoadListView.java
+++ app/src/main/java/cn/edu/uestc/acm/cdoj_android/layout/list/PullUpLoadListView.java
@@ -1,8 +1,7 @@
-package cn.edu.uestc.acm.cdoj_android.layout;
+package cn.edu.uestc.acm.cdoj_android.layout.list;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ListView;
@@ -13,8 +12,8 @@ import android.widget.ListView;
 public class PullUpLoadListView extends ListView {
 
     private boolean isPullUpLoading;
+    private boolean isPullUpLoad;
     private OnPullUpLoadListener onPullUpLoadListener;
-    private OnScrollListener onScrollListener;
     private PullUpLoadListViewFooter footer;
     public interface OnPullUpLoadListener {
         void onPullUpLoading();
@@ -36,41 +35,37 @@ public class PullUpLoadListView extends ListView {
     }
 
     private void init() {
-        isPullUpLoading = false;
-        footer = new PullUpLoadListViewFooter(getContext());
-        footer.setVisibility(View.VISIBLE);
-        addFooterView(footer);
-        onScrollListener = new OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (isPullUpLoad){
+            isPullUpLoading = false;
+            footer = new PullUpLoadListViewFooter(getContext());
+            footer.setVisibility(View.VISIBLE);
+            addFooterView(footer);
+            setOnScrollListener(new OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(AbsListView view, int scrollState) {
 
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (needLoad(firstVisibleItem, visibleItemCount, totalItemCount)) {
-                    Log.d("需要上拉加载", "onScroll: ");
-                    startPullUpLoad();
                 }
-            }
-            private boolean needLoad(int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                int lastVisibleItem = firstVisibleItem + visibleItemCount;
-                boolean isAtListEnd = lastVisibleItem == totalItemCount;
-                return (!isPullUpLoading && isAtListEnd);
-            }
-        };
-        setOnScrollListener(onScrollListener);
+
+                @Override
+                public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                    if (needLoad(firstVisibleItem, visibleItemCount, totalItemCount)) {
+                        startPullUpLoad();
+                    }
+                }
+                private boolean needLoad(int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                    int lastVisibleItem = firstVisibleItem + visibleItemCount;
+                    boolean isAtListEnd = lastVisibleItem == totalItemCount;
+                    return (!isPullUpLoading && isAtListEnd);
+                }
+            });
+        }
     }
 
     private void startPullUpLoad() {
-        Log.d("开始执行上拉加载", "startPullUpLoad: ");
         if (onPullUpLoadListener != null) {
             footer.updateView(PullUpLoadListViewFooter.State.LOADING, "Loading.....");
             isPullUpLoading = true;
-            Log.d("执行上拉加载", "startPullUpLoad: ");
             onPullUpLoadListener.onPullUpLoading();
-        }else {
-            Log.d("listener变空了", "startPullUpLoad: ");
         }
     }
 
@@ -80,11 +75,14 @@ public class PullUpLoadListView extends ListView {
 
     public void pullUpLoadingComplete() {
         isPullUpLoading = false;
-        Log.d("加载完成", "pullUpLoadingComplete: ");
         footer.updateView(PullUpLoadListViewFooter.State.NOT_LOADING,"Loading Complete");
     }
 
     public Boolean isPullUpLoading() {
         return isPullUpLoading;
+    }
+
+    public void setPullUpLoad(boolean isPullUpLoad) {
+        this.isPullUpLoad = isPullUpLoad;
     }
 }
