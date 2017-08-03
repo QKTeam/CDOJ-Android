4fd2286bd5149a40cf0bfa20753cc8e7210f8ca1
lagranmoon <tanliqun1999@163.com>
2017-08-03 10:05:51 +0800

    fix fatal

--- app/src/main/java/cn/edu/uestc/acm/cdoj/net/user/HandlerUser.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.uestc.acm.cdoj.net.user;
-
-/**
- * Created by lagranmoon on 2017/7/25.
- */
-
-public interface HandlerUser {
-
-}
---
+++ app/src/main/java/cn/edu/uestc/acm/cdoj/utils/SharedPreferenceUtil.java
@@ -0,0 +1,8 @@
+package cn.edu.uestc.acm.cdoj.utils;
+
+/**
+ * Created by lagranmoon on 2017/8/3.
+ */
+
+public class SharedPreferenceUtil {
+}
